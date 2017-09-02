9550b5da34fece707ae9c901c79984ddbaa642aa
a110482 <a110482@yahoo.com.tw>
2017-09-02 17:24:03 +0800

    231

--- abbott_demo_swift/PdfReadViewController.swift
+++ abbott_demo_swift/PdfReadViewController.swift
@@ -16,28 +16,22 @@ class PdfReadViewController: UIViewController {
     
     override func viewDidLoad() {
         self.title = file_name
-        establish_wev_view()
-        get_pdf(file_path: file_path!)
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         establish_wev_view()
-        web_view.backgroundColor = UIColor.red
         get_pdf(file_path: file_path!)
     }
     func establish_wev_view(){
         web_view.frame = self.view.frame
         web_view.center = self.view.center
+        web_view.center.y += 60
         self.view.addSubview(web_view)
     }
     func get_pdf(file_path:String){
         print("http://127.0.0.1:8000/pdf_dir/" + file_path)
-        //let file_path_part = file_path.components(separatedBy: ".")
-        //let sec_file_path = file_path_part[file_path_part.count - 1]
-        //let first_file_path = try! get_part_of_string(string: file_path, from: 0, before: (file_path.characters.count - sec_file_path.characters.count - 1))
-        //let pdf_loc = Bundle.main.url(forResource: (first_file_path), withExtension: sec_file_path)
         Http_Center().get_file("http://127.0.0.1:8000/pdf_dir/" + file_path) { (data:Data) in
-            let doc_path = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let doc_path = try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
             let doc_path2 = doc_path.appendingPathComponent(file_path)
             do {
                 // writing to disk
